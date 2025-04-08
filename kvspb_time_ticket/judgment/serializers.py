c773d6a234c494613def31033407c1e22c6fe7cb
semyon <orekhovsemyon@gmail.com>
2025-04-08 12:19:18 +0300

    fixed name, email entering

--- kvspb_time_ticket/judgment/serializers.py
+++ kvspb_time_ticket/judgment/serializers.py
@@ -1,13 +1,23 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from judgment.models import Judgment, Vacancy, District
 
 
+class InspectorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email",)
+
+
 class JudgmentSerializer(serializers.ModelSerializer):
+    inspector = InspectorSerializer(many=False)
 
     class Meta:
         model = Judgment
-        fields = '__all__'
+        fields = ("id_judgment", "district", "fio_judgment",
+                  "phone", "description", "inspector", "vacancies")
 
 
 class VacancySerializer(serializers.ModelSerializer):
